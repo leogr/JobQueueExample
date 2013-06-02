60e7a92f1c862e6ef7f63e2baa4ef94bd46c8e36
Leonardo Grasso <me@leonardograsso.com>
2013-06-02 13:58:06 +0200

    Initial setup

---
+++ Module.php
@@ -0,0 +1,50 @@
+<?php
+namespace JobQueueExample;
+
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getControllerConfig()
+    {
+        return array(
+            'factories' => array(
+
+            ),
+        );
+    }
+
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+
+             ),
+        );
+    }
+}
---
+++ src/JobQueueExample/Controller/ExampleController.php
@@ -0,0 +1,9 @@
+<?php
+namespace JobQueueExample\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class ExampleController extends AbstractActionController
+{
+
+}
